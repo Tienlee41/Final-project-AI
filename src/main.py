96ec45dc274200a9d7cabd4e7990b1329e5663ce
PHAMDANGPHONG <120189438+PHAMDANGPHONG@users.noreply.github.com>
2024-05-11 15:54:44 +0700

    22022614

--- src/main.py
+++ src/main.py
@@ -12,28 +12,31 @@ board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1], 'white')
 
 font = pygame.font.Font(None, 36)
 
+background_image = pygame.image.load("res/images/background.png")
+background_image = pygame.transform.scale(background_image, WINDOW_SIZE)
+
 def draw_start_menu():
-    screen.fill((255, 255, 255))
+    screen.blit(background_image, (0, 0))
 
     title_text = font.render('Chess', True, (0, 0, 0))
     title_rect = title_text.get_rect(center=(WINDOW_SIZE[0] // 2, 100))
     screen.blit(title_text, title_rect)
 
-    # Vẽ nút "Chơi với người"
+    # Vẽ nút "2 player"
     human_button = pygame.Rect((WINDOW_SIZE[0] // 2) - 100, 250, 200, 50)
     pygame.draw.rect(screen, (200, 200, 200), human_button)
     human_text = font.render('2 player', True, (0, 0, 0))
     human_text_rect = human_text.get_rect(center=human_button.center)
     screen.blit(human_text, human_text_rect)
 
-    # Vẽ nút "Chơi với máy"
+    # Vẽ nút "1 player"
     ai_button = pygame.Rect((WINDOW_SIZE[0] // 2) - 100, 350, 200, 50)
     pygame.draw.rect(screen, (200, 200, 200), ai_button)
     ai_text = font.render('1 player', True, (0, 0, 0))
     ai_text_rect = ai_text.get_rect(center=ai_button.center)
     screen.blit(ai_text, ai_text_rect)
 
-    # Vẽ nút "Thoát"
+    # Vẽ nút "Quit"
     exit_button = pygame.Rect((WINDOW_SIZE[0] // 2) - 100, 450, 200, 50)
     pygame.draw.rect(screen, (200, 200, 200), exit_button)
     exit_text = font.render('Quit', True, (0, 0, 0))
@@ -46,6 +49,7 @@ def draw(display):
 	board.draw(display)
 	pygame.display.update()
 
+# code chơi với người
 def play_with_human():
     running_game = True
     while running_game:
@@ -58,7 +62,7 @@ def play_with_human():
                     square_clicked = check_square_clicked(mx, my)
         draw(screen)
 
-
+# code chơi với máy
 def play_with_computer():
     running_game = True
     while running_game:
@@ -82,17 +86,15 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Nút trái chuột
                     mx, my = pygame.mouse.get_pos()
-                    # Kiểm tra xem người dùng có nhấn vào nút "Chơi với người" không
+                    # Kiểm tra xem người dùng có nhấn vào nút "2 player" không
                     if 250 <= my <= 300:
-                        #code chơi với người
                         play_with_human()
                         draw(screen)
-                    # Kiểm tra xem người dùng có nhấn vào nút "Chơi với máy" không
+                    # Kiểm tra xem người dùng có nhấn vào nút "1 player" không
                     elif 350 <= my <= 400:
-                        #code chơi với máy
                         play_with_computer()
                         draw(screen)
-                    # Kiểm tra xem người dùng có nhấn vào nút "Thoát" không
+                    # Kiểm tra xem người dùng có nhấn vào nút "Quit" không
                     elif 450 <= my <= 500:
                         running = False  # Dừng vòng lặp và thoát game
 
